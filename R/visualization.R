390836deb3f0dbc0de044452e2023490333cabb8
yexilein <stephischer@gmail.com>
2021-03-18 12:45:44 -0400

    Document all annotation-related functions

--- R/visualization.R
+++ R/visualization.R
@@ -5,8 +5,8 @@
 #' dense format.
 #' @param genes Vector of gene names.
 #' @param cell_type_label Vector of cell type names (must be same length as
-#' number of columns in expression_matrix)
-#' @return ggplot2 object
+#' number of columns in expression_matrix).
+#' @return A ggplot2 object.
 #'
 #' @export
 plot_marker_expression = function(expression_matrix, genes, cell_type_label) {
@@ -28,6 +28,18 @@ plot_marker_expression = function(expression_matrix, genes, cell_type_label) {
     return(result)
 }
 
+#' Plot markers scores in UMAP space (one facet per marker set).
+#'
+#' @param scores A marker score matrix as returned by score_cells or compute_marker_enrichment.
+#' @param umap_coordinates A (cell x 2) data.frame containing UMAP coordinates, where the ordering
+#'  of cells is the same as in the score matrix
+#' @param normalize_scores Boolean. If TRUE, marker scores are renormalized between
+#'  0 and 1 for all marker sets to obtain similar color ranges across facets.
+#' @param point_size Numeric value indicating the dot size.
+#' @param rasterize_umap Boolean. If TRUE, the UMAP part of the figure is rasterized.
+#'  This is particularly useful for large datasets.
+#' @return A ggplot2 object.
+#'
 #' @export
 plot_marker_scores = function(scores, umap_coordinates, normalize_scores=FALSE, point_size=1, rasterize_umap=FALSE) {
     colnames(umap_coordinates) = c("umap_1", "umap_2")
@@ -56,6 +68,18 @@ plot_marker_scores = function(scores, umap_coordinates, normalize_scores=FALSE, 
     return(result)
 }
 
+#' Plot cell type assignments in UMAP space.
+#'
+#' @param assignments A cell type assignment table as returned by assign_cells.
+#' @param umap_coordinates A (cell x 2) data.frame containing UMAP coordinates, where the ordering
+#'  of cells is the same as in the score matrix
+#' @param enrichment_threshold Numeric value defining a threshold below which a
+#'  cell should be considered unassigned (QC metric). 1 by default.
+#' @param point_size Numeric value indicating the dot size.
+#' @param rasterize_umap Boolean. If TRUE, the UMAP part of the figure is rasterized.
+#'  This is particularly useful for large datasets.
+#' @return A ggplot2 object.
+#'
 #' @export
 plot_assignments = function(assignments, umap_coordinates, enrichment_threshold = 1, point_size=1, rasterize_umap=FALSE) {
     colnames(umap_coordinates) = c("umap_1", "umap_2")
@@ -77,6 +101,14 @@ plot_assignments = function(assignments, umap_coordinates, enrichment_threshold 
     return(result)    
 }
 
+#' Compute UMAP coordinates for a target dataset.
+#'
+#' @param expr Expression matrix (with genes as rows) of the target dataset in
+#'  sparse or dense format. We recommend providing log-normalized counts for
+#'  carefully selected highly variable genes.
+#' @return A tibble with one row per cell and 3 columns containing the sample
+#'  name, UMAP coordinate 1 and UMAP coordinate 2.
+#'
 #' @export
 compute_umap = function(expr) {
     result = uwot::umap(t(as.matrix(expr))) %>%
